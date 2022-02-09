5a5da48027bf2f8f4d0a7acfcfefc0738c7dfe43
Francisco Carrasco Girbés <francagir@hotmail.com>
2022-02-09 10:25:23 +0100

    Start BackEnd

---
+++ GetDate.php
@@ -0,0 +1,3 @@
+<?php
+    echo "Hello, Today is ". date("Y-m-d H:i:s");
+?>
---
+++ PHP_Connection.php
@@ -0,0 +1,30 @@
+<?php 
+
+    $servername = "localhost";
+    $username = "username";
+    $password = "password";
+    $dbname = "databasename"
+
+    //Create Con
+    $conn = new mysqli( $servername,$username,$password,$dbname);
+    
+    //Check Connection
+    if($conn-> connect_error)
+        die("Connection Failed: " . $conn->connect_error)
+
+    echo "Connected Succesfully";
+   
+    $sqlQuery = "SELECT id, genre, age, userlocation, phobiaLevel, symptoms FROM users";
+    
+    $result = $conn->query($sql);
+
+    if($result->num_rows>0){
+        while($row = $result->fetch_assoc()){
+            echo "id: " .$row["id"].  " -Genre: " .$row["genre"].  " -age: " .$row["age"].  " -location: " .$row["userlocation"].  " -phobialevel: " .$row["phobiaLevel"].  " -symptoms: " .$row["symptoms"]. "<br>";
+        
+        }
+    }else{
+        echo "0 results"
+    }
+$conn -> close();
+?>
