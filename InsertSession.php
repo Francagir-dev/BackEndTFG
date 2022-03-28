73aeb249e6f595c55db86096e0114f9dcf77664d
Francisco Carrasco Girbés <60694098+Francagir-dev@users.noreply.github.com>
2022-03-28 19:46:27 +0200

    Updated connections

---
+++ InsertSession.php
@@ -0,0 +1,57 @@
+<?php 
+
+    $servername = "localhost";
+    $username = "username";
+    $password = "password";
+    $dbname = "SessionInfo";
+    //Create Con
+    $conn = new mysqli( $servername,$username,$password,$dbname);
+    
+    $sessionData = $_POST["sessionDataJson"];
+     
+    // Decoding json 
+    $sessionDecoded = json_decode($sessionData);
+
+    //Desencrypt (?)
+    $method = 'aes-256-cbc';
+
+    $iv = base64_decode("C9fBxl1EWtYTL1/M8jfstw==");
+
+    $desencriptar = function ($valor) use ($method, $clave, $iv) {
+        $encrypted_data = base64_decode($valor);
+        return openssl_decrypt($valor, $method, $clave, false, $iv);
+    };
+
+    $getIV = function () use ($method) {
+        return base64_encode(openssl_random_pseudo_bytes(openssl_cipher_iv_length($method)));
+
+        //Desencrypted JSON
+    $sessionDesencrypted =  $desencriptar($sessionDecoded);
+
+    
+    //Assign Values
+    $genre = $sessionDesencrypted -> genre;
+    $age =  $sessionDesencrypted -> age;
+    $phobiaLevel = $sessionDesencrypted -> phobiaLevel;
+    $symptoms =  $sessionDesencrypted -> symptoms;
+    $HRV =  $sessionDesencrypted -> HRV;
+    $duration =  $sessionDesencrypted -> durationSession;
+    $date = .date("dd-M-YY H:i:s");
+
+
+ //Check Connection
+ if($conn-> connect_error)
+     die("Connection Failed: " . $conn->connect_error)
+     $sql = "INSERT INTO sessionInfo (genre, age, userLocation, phobiaLevel, symptoms, HRV, duration, date) 
+      VALUES('".$genre ."',
+             '" .$age ."',
+             '" .$phobiaLevel ."',
+             '" .$symptoms ."',
+             '" .$HRV ."',
+             '" .$duration ."',
+             '" .$date ."')";
+
+
+
+
+?>
--- PHP_Connection.php
+++ PHP_Connection.php
@@ -13,19 +13,47 @@
         die("Connection Failed: " . $conn->connect_error)
 
     echo "Connected Succesfully";
-   
-    $sqlQuery = "SELECT id, genre, age, userlocation, phobiaLevel, symptoms, HRV FROM users";
+    /*
+    $sqlQuery = "SELECT id, genre, age, userlocation, phobiaLevel, symptoms, HRV FROM SessionInfo";
+    $sqlQuery = "SELECT id, genre, age, userlocation, phobiaLevel, symptoms, duration FROM SessionInfo";
+    */
+    $sqlQuery = "SELECT id, genre, age, userlocation, phobiaLevel, symptoms, HRV, duration FROM SessionInfo";
     
-    $result = $conn->query($sql);
 
+    $result = $conn->query($sql);
+    $sessionData;
+    $json_array = array();
     if($result->num_rows>0){
         while($row = $result->fetch_assoc()){
-            echo "id: " .$row["id"].  " -Genre: " .$row["genre"].  " -age: " .$row["age"].  " -location: " .$row["userlocation"].  " -phobialevel: " .$row["phobiaLevel"]. 
-             " -symptoms: " .$row["symptoms"].  " -HRV: " .$row["HRV"]. "<br>";
-        
+            
+            echo 
+            " id: " .$row["id"].  
+            " - Genre: " .$row["genre"].  
+            " - Age: " .$row["age"].  
+            " - Location: " .$row["userLocation"].  
+            " - Phobia Level: " .$row["phobiaLevel"]. 
+            " - Symptoms: " .$row["symptoms"].  
+            " - HRV: " .$row["HRV"]. 
+            " - Duration: " .$row["duration"].
+            " - Date: " .$row["date"]
+            "<br>";
         }
     }else{
         echo "0 results"
     }
+/*
+    if($result->num_rows>0){
+        while($row = $result->fetch_assoc()){
+            echo "id: " .$row["id"].  " -Genre: " .$row["genre"].  " -age: " .$row["age"].  " -location: " .$row["userlocation"].  " -phobialevel: " .$row["phobiaLevel"]. 
+             " -symptoms: " .$row["symptoms"].  " -Duration: " .$row["duration"]. "<br>";
+        }
+    }else{
+        echo "0 results"
+    }
+*/
+
+
 $conn -> close();
+
+function SetClassForJson(){}
 ?>
