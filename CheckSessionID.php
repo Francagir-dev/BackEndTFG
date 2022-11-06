90f9c71b1c8b7566ad077d6d1c4e9b6dff7644f5
Francisco Carrasco Girbés <60694098+Francagir-dev@users.noreply.github.com>
2022-11-06 13:35:19 +0100

    Comment CheckSessionID

--- CheckSessionID.php
+++ CheckSessionID.php
@@ -1,8 +1,8 @@
 <?php 
 
-    include 'ConexionDB.php';
+    include 'ConexionDB.php'; //Class containing all DB info
 
-    $sesionCode = $_POST["codeSession"];
+    $sesionCode = $_POST["codeSession"]; //Variable that will receive info from the app
 
     $sqlQuery = "SELECT session.ID as SessionID, session.sessionCode as SessionCode, session.idPatientSpecialist as SessionIDSP, 
     patienthasspecialists.patientID as PatientID, patient.name as PatientName, patient.phobia as PatientPhobia, patient.anxietyLVL as PhobiaLevel   
@@ -10,19 +10,19 @@
       INNER JOIN patienthasspecialists ON session.idPatientSpecialist = patienthasspecialists.ID
       INNER JOIN patient ON patienthasspecialists.patientID = patient.ID
       where sessionCode = '".$sesionCode."'
-      GROUP BY session.sessionCode, patienthasspecialists.patientID";
+      GROUP BY session.sessionCode, patienthasspecialists.patientID"; //Query that will return info from different tables (Session, patienthasspecialists, patient, specialist)
 
-    $result = $conn->query($sqlQuery);
+    $result = $conn->query($sqlQuery); //Getting the result
 
-if($result->num_rows>0){
-   $rows = array();
-   while($row = $result->fetch_assoc()){
+if($result->num_rows>0){//if there is at least 1
+   $rows = array(); //Array of info
+   while($row = $result->fetch_assoc()){//while there is any row remaining
        $rows[] = $row;
    }    
-   echo json_encode($rows);
-}else{
+   echo json_encode($rows);//return info 
+}else{//If query don't find any
    echo "Error 404";
 }
-$conn->close();
+$conn->close();//Close connection
 
 ?>
